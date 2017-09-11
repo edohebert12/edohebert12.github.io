8f8d59344aa50297693461a620decd4f88bb4522
Edouard Hebert <edohebert12@gmail.com>
2017-09-11 14:07:14 -0400

    c1

---
+++ factweb.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript">
+    document.write([
+        "\<script src='",
+        ("https:" == document.location.protocol) ? "https://" : "http://",
+        "ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js' type='text/javascript'>\<\/script>" 
+    ].join(''));
+
+function popup(page) {
+	var s=''
+	while  (s.length<10) {
+  	t = (48 + Math.round(Math.random() * 74))
+  	r = ((t<58) || ((t>64) && (t<91)) || ((t>96) && (t<123))) ? String.fromCharCode(t) : ''
+  	s += r
+  	}
+	//alert(s);
+	window.open(page,s,"width=775,height=600,screenX=0,screenY=0,top=0,left=0,location=no,statusbar=yes,toolbar=yes,scrollbars=yes,titlebar=no,resizable=yes");
+}
+
+
+</script>
+
+<title>FactWeb</title>
+<style type="text/css">
+<!--
+body {
+	margin-top: 0px;
+	background-color: #FFFFFF;
+}
+.main_text {
+	font-family: "Segoe UI", "Times New Roman";
+	font-size: 16px;
+	color: #000000;
+	font-style: italic;
+}
+.style3 {color: #FF0000}
+.style10 {
+	font-family: "Segoe UI", "Times New Roman";
+	font-size: 11px;
+}
+.style12 {font-style: italic; color: #000000;}
+
+button {
+  width:111px;
+  height:91px;
+  float:left;
+  border:none;
+  margin:0.9em;
+}
+.small_button {
+  width:111px;
+  height:46px;
+}
+button:hover {
+    margin-top: 0.2em;
+    -moz-box-shadow: 0px 0px 4px 1px grey;
+    -webkit-box-shadow: 0px 0px 4px 1px grey;
+    box-shadow: 0px 0px 4px 1px  grey;
+}
+-->
+</style>
+
+
+
+</head>
+
+<body>
+<div style="max-width:1000px; border-left:#000000 1px solid; border-right:#000000 1px solid; margin:auto;">
+
+<div style="max-width:1000px; background-image: url(factweb_bg_top.jpg); height: 25px; margin: auto;"></div>
+<div style="max-width:1000px; margin-left: auto; margin-right: auto; text-align: center; background-color: #FFFFFF;"><a href=""><img src="factweb_title.jpg" border="0" align="middle" /></a></div>
+<div style="max-width:1000px; background-image: url(factweb_bg_middle.jpg); height: 12px; margin-left: auto; margin-right: auto;"></div>
+<div style="max-width:960px; text-align: center; background-color: #FFFFFF; padding: 20px; margin: auto;">
+  <span class="main_text" style="clear:left;"><br />
+  The <b>FACT-Web</b> programs offer free but<span class="style3"> very limited</span> interaction 
+              with <strong>FactSage</strong>. This service may be discontinued 
+              at any time without notice. The programs are offered for demonstration 
+purposes only. Many of the features available in the full <b>FactSage</b> package have been removed or diluted.</span><br />
+<br />
+<a href="javascript:popup('http://www.crct.polymtl.ca/compweb.php');">
+<button style="background-image:url(factweb_compound_button.jpg);" onmouseover="$('#button_description').text('List of the pure substances in FactPS - the FACT pure substances database. \
+NEW! ViewData lists tables of thermodynamic data and phase transitions.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+<a href="javascript:popup('http://www.crct.polymtl.ca/reacweb_plus.php');">
+<button style="background-image:url(factweb_reaction_button.jpg);" onmouseover="$('#button_description').text('Properties of a species or chemical reaction. NEW! Advanced calculations where chemical\ reactions may be non-isothermal, non-standard state, adiabatic, high pressure, etc.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+<a href="javascript:popup('http://www.crct.polymtl.ca/aquaweb.html');">
+<button style="background-image:url(factweb_aqualib_button.jpg);" onmouseover="$('#button_description').text('Chemical equilibria calculation of a dilute aqueous solution with pure solids.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+<a href="javascript:popup('http://www.crct.polymtl.ca/equiwebmenu.php');">
+<button style="background-image:url(factweb_equilib_button.jpg);" onmouseover="$('#button_description').text('Chemical equilibria calculations of systems including gases, pure substances and real\ solutions.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+<a href="javascript:popup('http://www.crct.polymtl.ca/predomweb.php');">
+<button style="background-image:url(factweb_predom_button.jpg);" onmouseover="$('#button_description').text('Isothermal predominance area diagram.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+<a href="javascript:popup('http://www.crct.polymtl.ca/ephweb.php');">
+<button style="background-image:url(factweb_eph_button.jpg);" onmouseover="$('#button_description').text('Eh vs pH Pourbaix diagram.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+<a href="javascript:popup('http://www.crct.polymtl.ca/fact/pdweb.php');">
+<button style="background-image:url(factweb_pd_button.jpg);" onmouseover="$('#button_description').text('Interactive phase diagrams and search engine.');" onmouseout="$('#button_description').text('');"></button>
+</a>
+
+<br />
+<div id="button_description" class="main_text" style="clear:left; margin:1em;  height:80px;  display:block;  " >
+
+</div>
+
+
+</div>
+<div style="max-width:1000px; margin-left: auto; margin-right: auto;">
+<div style="display:inline-block; height:229px; max-width:148px; float:left;"><img src="factweb_bottom_left.jpg" border=0 /></div>
+<div class="style10" style="background-image:url(factweb_bottom_bg.jpg); padding-left:20px; padding-top:5px; padding-right:10px; width:822px; height:224px; display:inline-block;">
+<u>FactSage:</u><br>
+<br>
+To cite FactSage in a publication, please use the following:<br />
+
+C. W. Bale, E. B&eacute;lisle, P. Chartrand, S. A. Decterov, G. Eriksson, A.E. Gheribi, K. Hack, I. H. Jung, Y. B. Kang, J. Melan&ccedil;on, A. D. Pelton, S. Petersen, C. Robelin. J. Sangster and M-A. Van Ende, <em><a href="http://www.crct.polymtl.ca/dl/FactSageIII.pdf" target="_blank">FactSage Thermochemical  Software and Databases, 2010-2016</a></em>, Calphad, vol. 54, pp 35-53,  2016 &lt;www.factsage.com&gt;<br />
+<br />
+<u> Fact-Web:</u><br>
+<br />
+If you employ the  results of <span class="style12">Fact-Web</span> calculations in a publication make sure that you are  aware of the limitations of the calculation.&nbsp;  This is particularly important in <strong>Equilib-Web</strong> which does  not have access to real solution data. <br>
+<br>
+To cite <strong>Fact-Web</strong> or one of its modules in a publication please use the following reference: <br />
+
+C. W. Bale and E. B&eacute;lisle, <em>Fact-Web suite of interactive programs</em>, www.factsage.com            
+</div>
+</div>
+
+
+
+</div>
+
+</body>
+</html>

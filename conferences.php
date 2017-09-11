da48df938ddafe35c5a2eacb9625520f03c1d7f0
Edouard Hebert <edohebert12@gmail.com>
2017-09-11 14:23:56 -0400

    c1

---
+++ conferences.php
@@ -0,0 +1,107 @@
+
+<html>
+<head>
+<title>Conference and Workshops</title>
+
+<style>
+  <!--
+A:link    {color:#0033CC;  text-decoration: none}
+A:visited {color:#993300;  text-decoration: none}
+A:hover   {color:#000066;  text-decoration: none; background-color: lightgrey}
+.link {text-decoration:none;}
+.style1 {font-family: Arial, Helvetica, sans-serif}
+.style3 {font-family: Arial, Helvetica, sans-serif; font-size: 12px; }
+  -->
+</style>
+
+</head>
+<body bgcolor=white link=#B7AD81 slink=#A22723 vlink=#847B52 font="Lucida Sans Unicode, Verdana, Tahoma, Arial">
+<table width="605" border="0" cellpadding="1" cellspacing="0" bgcolor="#000066">
+  <tr>
+    <td><table width="100%" border="0" cellpadding="5" cellspacing="0" bgcolor="#FFFFFF">
+  <tr><td>
+<div align="center"> 
+  <p align="center"><font face="Trebuchet MS, Arial" color="#0B075E" size="4"><b>Conferences 
+    and Workshops<br>
+    <img src="spacer.jpg" width="409" height="16"> </b></font></p>
+  <p align="center"><font face="Trebuchet MS, Arial"><img src="tree4.gif" width="32" height="32" border="0"><font color="#0B075E"><br>
+    </font></font></p>
+            <table border="0" cellspacing="0" cellpadding="0">
+              <tr> 
+                <td colspan="2" bgcolor="cccccc"><span class="style1"><font color="#0B075E" size="3"><b>FactSage 
+                  Conferences and Workhops</b></font></span></td>
+              </tr>
+              <tr> 
+                <td height="5" colspan="2"><span class="style1"><img src="spacer.gif" width="2" height="2"></span></td>
+              </tr>
+	<tr>
+                <td colspan="2"><span class="style3">Download "<a href="http://www.crct.polymtl.ca/dl/TMS_2016_FactSage.pdf" target="_blank">FactSage - Past, Present and Future</a>" - includes a history of FactSage and photos of the current FactSage developers. Presented at TMS Symposium held in Nashville, 2016.</span><br></td>
+              </tr>
+              <tr>
+                <td><span class="style1"><font color="#0B075E" size="-1"><a><br>
+                </a></font></span></td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td colspan="2"><span class="style3">Download &quot;<a href="TMS_2014_Robertson_Symposia.pdf">Recent Developments in FactSage Thermochemical Software and Databases</a>&quot; presented at <br>
+                'EPD Symposium in Honor of David G.C. Robertson', TMS Annual Meeting, San Diego, 2014.</span><br></td>
+              </tr>
+              <tr>
+                <td><span class="style1"><font color="#0B075E" size="-1"><a><br>
+                </a></font></span></td>
+                <td>&nbsp;</td>
+              </tr>
+         
+              <tr> 
+                <td colspan="2" bgcolor="cccccc"><span class="style1"><font color="#0B075E" size="3"><b>Other</b></font></span></td>
+              </tr>
+              <tr> 
+                <td colspan="2"><font face="Lucida Sans Unicode, Verdana, Tahoma, Arial" color="#0B075E"><font size="2"><font color="#999999"><a><a href="http://www.calphad.org" target="_blank"><span class="style1"><font size="-1"><b><u>June 11-16</u></b></font></span><font size="-1"><span class="style1"><b><u> 2017 - CALPHAD XLVI, Saint-Malo, France</u></b></span></font></a></a> 
+                  </font></font></font></td>
+              </tr>
+              <tr> 
+                <td><span class="style1"><font color="#0B075E" size="-1"><a><br>
+                  </a></font></span></td>
+                <td><div align="justify" class="style1"> <font color="#0B075E"><font size="2">CALPHAD 
+                     - An International Conference on Phase Diagram Calculations 
+                    and Computational Thermochemistry.</font></font></div></td>
+              </tr>
+              <tr> 
+                <td height="5" colspan="2"><font face="Trebuchet MS, Arial"><img src="spacer.gif" width="2" height="2"></font></td>
+              </tr>
+              <tr> 
+                <td height="5" colspan="2">&nbsp;</td>
+              </tr>
+              <tr> 
+                <td bgcolor="#FFFFFF" colspan="5"><div align="center"> <font face="Trebuchet MS, Arial"> 
+                    </font>
+<table width="100%">
+  <tr> 
+    <td colspan="3"><hr></td>
+  </tr>
+  <tr>
+    <td><font color="#666666" size="1" face="Trebuchet MS, Arial">Last updated: 
+      February 6 2017 , <a href="mailto:cbale@polymtl.ca">Christopher 
+      W. Bale</a> &amp; <a href="mailto:eve.belisle@polymtl.ca">Eve B&eacute;lisle</a></font></td>
+    <td><div align="center"></div></td>
+    <td><div align="right"><font color="#666666" size="1" face="Trebuchet MS, Arial"><a href="http://www.crct.polymtl.ca" target="_blank">www.crct.polymtl.ca</a> - <a href="http://www.factsage.com">www.factsage.com </a></font></div></td>
+  </tr>
+</table>
+<font face="Trebuchet MS, Arial">
+</font>                    </font></div></td>
+              </tr>
+            </table>
+  </td></tr></table></td></tr></table>
+  
+  <!-- Start of StatCounter Code -->
+<script type="text/javascript" language="javascript">
+var sc_project=520806; 
+var sc_partition=3; 
+var sc_invisible=1; 
+</script>
+
+<script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script><noscript><a href="http://www.statcounter.com/" target="_blank"><img  src="http://c4.statcounter.com/counter.php?sc_project=520806&amp;java=0&amp;invisible=1" alt="html hit counter" border="0"></a> </noscript>
+<!-- End of StatCounter Code -->
+
+  </body>
+</html>

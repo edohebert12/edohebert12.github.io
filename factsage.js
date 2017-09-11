ecd047a5bb8bb59803769577d18b4be86d62d64b
Edouard Hebert <edohebert12@gmail.com>
2017-09-11 12:47:54 -0400

    c1

--- factsage.js
+++ factsage.js
@@ -2,14 +2,9 @@ function loadXML() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			console.log("yep");
 		    document.getElementById("php-content").innerHTML = this.responseText;
-		}
-		else{
-			console.log("nope");
 		}
 	};
 	xhttp.open("GET", "fs_general.php", true);
 	xhttp.send();
-	document.getElementById("php-content").innerHTML = "LOL";
 }

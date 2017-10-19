bd4fa805bdb73ae98c3dcb7d3d139a93127fd172
Edouard Hebert <edohebert12@gmail.com>
2017-10-19 17:22:03 -0400

    Put map in

--- index.js
+++ index.js
@@ -26,3 +26,7 @@ $("#recherche").autocomplete({
 });
 $("#recherche").autocomplete("widget").addClass("autocomplete-results");
 
+var map = new google.maps.Map(document.getElementById("map"), {
+  center: {lat: -34.397, lng: 150.644},
+  zoom: 8
+});

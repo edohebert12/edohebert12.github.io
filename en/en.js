07899a62cd28c2be9aa44543ab9a7b6e06b7600e
Sébastien Labine <sebastienlabine@aol.com>
2017-10-29 01:55:15 -0400

    Ajout du loading screen et fix des bugs

--- en/en.js
+++ en/en.js
@@ -3,116 +3,112 @@ var map;
 var stations;
 var marker;
 var table;
-
-$(selector).on('click', function(){
-    $(selector).removeClass('active');
-	$(this).addClass('active');
-	if(this.id === "liste") {
-		unloadSection1();
-		loadSection2();
-	}
-	else {
-		loadSection1();
-		unloadSection2();
-	}
-});
-
-function loadSection1() {
-	$('.section1').show();
-}
-
-function unloadSection1() {
-	$('.section1').hide();
-}
-
-function loadSection2() {
-	$.ajax({
-		url: 'https://secure.bixi.com/data/stations.json',
-		dataType: 'json',
-		success: function(data){
-			var tableBody = $('#liste-des-stations')[0].getElementsByTagName('tbody')[0]
-			tableBody.innerHTML = "";
-			for(var index in data.stations) {
-				var s = data.stations[index];
-				tableBody.innerHTML += "<tr><td>" + s.id.toString() + "</td><td>" + s.s + "</td><td>" + s.ba + "</td><td>" + s.da + "</td><td>" + (s.b ? "yea" : "no") + "</td><td>" + (s.su ? "yes" : "no") + "</tr>";
-			}
-			if(table != undefined) {
-				table.destroy();
-			}
-			table = $('#liste-des-stations').DataTable({"pageLength": 10,"dom": '<"top"f>rt<"bottom"<"table-page"li><"page-numbers"p>><"clear">'});
-			$(".bottom").css("width", $("#liste-des-stations").width());
-			$(".table-page").css("float", "left");
-			$(".page-numbers").css("float", "right");
-		},
-		error: function(a, b){
-			console.log(b);
-		}
-	});
-	$('.section2').show();
-}
-
-function unloadSection2() {
-	$(".section2").hide();
-}
-
-$(".progress").css("width", ($(".table-bordered").width() + 2).toString() + "px");
-
+var isLoaded = false;
+$('.loading').show();
+/* Information pas clair, donc nous avons décidé de chargé une seule fois le json avec ajax 
+et de réutilisé ces informations afin d'augmenté la performance. De plus, l'API ne nous permet pas de filter les données sur la request get, donc 
+ce n'est pas performant de chargé toujours le json */
 $.ajax({
 	url: 'https://secure.bixi.com/data/stations.json',
 	dataType: 'json',
 	success: function(data){
-		stations = $.map(data.stations, function(item){ return item.s; });
+		stations = $.map(data.stations, function(item){ return item});
+		stationsNames = $.map(data.stations, function(item){ return item.s});
+		loadTable();
+		$('.loading').hide();
 	}
 });
 
+/* Changement d'onglets */
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+	$(this).addClass('active');
+	if(this.id === "liste") {
+		showTable();
+	}
+	else {
+		showListe();
+	}
+});
+
+/* Load la table */
+function loadTable() {
+	if(!isLoaded){
+		var tableBody = $('#liste-des-stations')[0].getElementsByTagName('tbody')[0]
+		tableBody.innerHTML = "";
+		for(var index in stations) {
+			var station = stations[index];
+			tableBody.innerHTML += "<tr><td>" + station.id.toString() + "</td><td>" + station.s + "</td><td>" + station.ba + "</td><td>" + station.da + "</td><td>" + (station.b ? "Yes" : "No") + "</td><td>" + (station.su ? "Yes" : "No") + "</tr>";
+			isLoaded = true;
+		}
+	}
+}
+/* Affiche la table */
+function showTable(){
+	if(table != undefined) {
+				table.destroy();
+		}
+		$('table').css('width','90vw');
+
+		table = $('#liste-des-stations').DataTable({"pageLength": 10,"dom": '<"top"f>rt<"bottom"<"table-page"li><"page-numbers"p>><"clear">'});
+		$(".bottom").css("width", $("#liste-des-stations").width());
+		$(".table-page").css("float", "left");
+		$(".page-numbers").css("float", "right");
+
+		$('.section1').hide();
+		$('.section2').show();
+};
+
+function showListe() {
+	/* Devrais être dans une box avec la progress bar */
+	$('table').css('width','57.5vw');
+	$('.section1').show();
+	$('.section2').hide();
+}
+
+/* Auto complete */
 $("#recherche").autocomplete({
     source : function(request, reponse){
-		reponse($.ui.autocomplete.filter(stations,$('#recherche').val()));
+		reponse($.ui.autocomplete.filter(stationsNames,$('#recherche').val()));
 	},
 	minLength: 1,
 	select: function(event,ui) {
 		$(".localisation .station").text(ui.item.label);
-		$.ajax({
-			url: 'https://secure.bixi.com/data/stations.json',
-			dataType: 'json',
-			success: function(data){
-				var station = data.stations.find(function(item){
-					return item.s === ui.item.label;
-				});
-				var pos = new google.maps.LatLng(station.la, station.lo);
-				map.panTo(pos);
-				map.setZoom(16);
-				if(marker == undefined) {
-					marker = new google.maps.Marker({map: map});
-				}
-				updateNameColor("#station-id",station.id);
-				updateNameColor("#blq",station.b)
-				updateNameColor("#velos-dispo",station.ba);
-				updateNameColor("#bornes-dispo",station.da);
-				updateNameColor("#susp",station.su);
-				updateNameColor("#velos-indispo",station.bx);
-				updateNameColor("#hs",station.m)
-				updateNameColor("#bornes-indispo",station.dx)
-	
-				marker.setPosition(pos);
-			}
+		var station = stations.find(function(item){
+			return item.s === ui.item.label;
 		});
+		var pos = new google.maps.LatLng(station.la, station.lo);
+		map.panTo(pos);
+		map.setZoom(16);
+		if(marker == undefined) {
+			marker = new google.maps.Marker({map: map});
+		}
+		updateNameColor("#station-id",station.id);
+		updateNameColor("#blq",station.b)
+		updateNameColor("#velos-dispo",station.ba);
+		updateNameColor("#bornes-dispo",station.da);
+		updateNameColor("#susp",station.su);
+		updateNameColor("#velos-indispo",station.bx);
+		updateNameColor("#hs",station.m)
+		updateNameColor("#bornes-indispo",station.dx)
+		marker.setPosition(pos);
+			
 	}
 });
 $("#recherche").autocomplete("widget").addClass("autocomplete-results");
 
 function updateNameColor(selector,value){
 	if(typeof(value) === "boolean"){	
-		if(value == false){
-			$(selector).text("No");
+		if(value==false){
+			$(selector).text("Non");
 			$(selector).css("background-color", "green");
 		} else{
-		$(selector).text("Yes");
+		$(selector).text("Oui");
 		$(selector).css("background-color", "red");		
 		}
 	}else{
 		if(selector == "#velos-dispo" || selector == "#bornes-dispo"){
-			if(value <= 3) {
+			if(value == 0) {
 				$(selector).css("background-color", "red");
 			} else {
 				$(selector).css("background-color", "green");
